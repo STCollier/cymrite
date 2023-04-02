f4aa4ba024311f1b8d80fb62308c5da4db803a61
STCollier <1samuel3collier@gmail.com>
2023-04-02 18:16:21 -0400

    Broken compiling

--- tests/test.c
+++ tests/test.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "../include/cymrite/array.h"
 
 int main(void) {
-    printf("%s\n", "Hello Cymrite!");
+    struct Array myArray = createArray();
+
+    myArray = arrayInsert(myArray, 0, "e");
+
+    printf("%c", myArray);
 
     return 0;
 }
