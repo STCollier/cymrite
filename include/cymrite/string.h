65999068c5a945405b7031afb6b52de25e14680c
Eczbek <eczbek.void@gmail.com>
2023-05-25 16:29:23 -0400

    Fix redefinition of cymrite_whitespace

--- include/cymrite/string.h
+++ include/cymrite/string.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-const char* cymrite_whitespace = "\x9\xa\xb\xc\xd\x20\x85\xa0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202F\u205f\u3000";
+extern const char* cymrite_whitespace;
 
 size_t cymrite_countTokens(const char* string);
 
